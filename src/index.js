e2680e828afd6be7f1055db94bfc2bcd73c227c2
Sean <seanpoulter@hotmail.com>
2022-08-19 15:15:57 -0400

    Import style.css added

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import './style.css';
+
+const content = document.querySelector('#content');
+const header = document.createElement('h1');
+const homeTab = document.createElement('div');
+const menuTab = document.createElement('div');
+const contactTab = document.createElement('div');
+const navigateDiv = document.createElement('div');
+
+content.appendChild(header);
+content.appendChild(navigateDiv);
+navigateDiv.className = "navigate";
+navigateDiv.appendChild(homeTab);
+navigateDiv.appendChild(menuTab);
+navigateDiv.appendChild(contactTab);
+
+header.textContent = "Fabulous food stuff emporium";
+homeTab.textContent = "Home";
+menuTab.textContent = "Menu";
+contactTab.textContent = "Contact";
+
